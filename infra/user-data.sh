1187ec59435c969ddd5915f31bfd59c6e0f70707
tanay0505 <tanay3527@gmail.com>
2026-06-23 03:10:20 +0000

    Second commit

--- infra/user-data.sh
+++ infra/user-data.sh
@@ -10,6 +10,6 @@ cd /home/ec2-user
 
 git clone https://github.com/tanay0505/Zero-Key-Deploy.git
 
-cp /home/ec2-user/Git/app/index.html /usr/share/nginx/html/index.html
+cp /home/ec2-user/Zero-Key-Deploy/app/index.html /usr/share/nginx/html/index.html
 
 systemctl restart nginx
